2036116ddde177f8857d7a875aee61e514e606d3
jennalehn <jenna.lehn@gmail.com>
2020-10-14 15:24:02 -0400

    made it prettier

--- Git7.R
+++ Git7.R
@@ -8,18 +8,26 @@ iris
 # print last 2 rows in last 2 columns
 tail(iris[,c(4,5)], 2)
 # get # of observations for each species included in data set
-unique(iris$Species)
-nrow(iris[iris[,5]=="setosa",])
-nrow(iris[iris[,5]=="versicolor",])
-nrow(iris[iris[,5]=="virginica",])
+unique(iris$Species) #find names of each species
+obssetosa=sum(iris$Species=="setosa")
+paste("There are",obssetosa,"observations of the setosa species.")
+obsversicolor=sum(iris$Species=="versicolor")
+paste("There are",obsversicolor,"observations of the versicolor species.")
+obsvirginica=sum(iris$Species=="virginica")
+paste("There are",obsvirginica,"observations of the virginica species.")
 # get rows with Sepal.Width > 3.5
-iris[iris[,2]>3.5,2]
-# write data for species setosa to setosa.csv
-setosa.csv=iris[iris[,5]=="setosa",]
+bigwidth=iris[iris[,2]>3.5,]
+bigwidth
+# write data for species setosa to comma deliminated file
+setosa=iris[iris[,5]=="setosa",]
+write.csv(setosa, file="setosa.csv")
 setosa.csv
 # calculate mean, min, max of petal.length from virginica
 virginica=iris[iris[,5]=="virginica",]
 virginica
-mean(virginica$Petal.Length)
-min(virginica$Petal.Length)
-max(virginica$Petal.Length)
+meanv=mean(virginica$Petal.Length)
+paste("The mean of virginica petal length is",meanv)
+minv=min(virginica$Petal.Length)
+paste("The minimum petal length for virginica is",minv)
+maxv=max(virginica$Petal.Length)
+paste("The maximum petal length for virginica is",maxv)
