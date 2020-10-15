8656d14bb524086bc00375071135a57a7154654b
Kyle Walker <kwalke11@nd.edu>
2020-10-15 10:51:01 -0400

    Exercise answers

--- Biocomputing_Exercise07_Script.R
+++ Biocomputing_Exercise07_Script.R
@@ -6,12 +6,12 @@ x[1:y,] #returns "y" lines from "x"
 
 
 iris <- read.csv("iris.csv") # start of problem 2
-tail(iris,2)[,4:5]
-table(iris$Species)
-subset(x=iris, iris$Sepal.Width > 3.5)
-setosa <- subset(x=iris, iris$Species == "setosa")
-write.csv(setosa, file="setosa.csv")
-virginica <- subset(x=iris, iris$Species =="virginica")
+tail(iris,2)[,4:5] #returns last 2 rows of last 2 columns
+table(iris$Species) #shows the number of samples for each species
+subset(x=iris, iris$Sepal.Width > 3.5) #shows all observations that had Sepal Width >3.5 
+setosa <- subset(x=iris, iris$Species == "setosa") #creates a subset that is only observations for setosa
+write.csv(setosa, file="setosa.csv") #creates a .csv file for setosa observations
+virginica <- subset(x=iris, iris$Species =="virginica") #creates a subset that is only observationa for virginica
 summary(virginica$Petal.Length) #this also gives 1st Q, median and 3rdQ
 mean(virginica$Petal.Length) # can also use the following 3 commands 
 min(virginica$Petal.Length)
