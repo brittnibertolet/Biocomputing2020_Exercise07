e983f286bda46f38a897040d8c5e4c3802bfdeb4
kmichael4324 <71276752+kmichael4324@users.noreply.github.com>
2020-10-15 13:56:09 -0400

    Add files via upload

---
+++ Exercise_07.R
@@ -0,0 +1,16 @@
+#Exercise_07 
+#Kevin, Lucia, Austin
+
+#Number One
+#set the file you would like to pull the amount of lines from as the var
+#your_file and read in your file 
+your_file <- "iris.csv"
+setwd("/Users/kevinmichael/Desktop/BioComp/data-shell/Biocomputing2020_Exercise07")
+df <- read.table(file=your_file, header=T, sep=" ")
+
+#now, set how many lines you would like to see
+#we use 5 as an arbitrary example
+lines <- 7
+head <- df[1:lines, ]
+#now print 
+print(head)
