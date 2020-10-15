65247a700c13d0e4442aa8d00b4019662a91b75d
kmichael4324 <71276752+kmichael4324@users.noreply.github.com>
2020-10-15 14:24:21 -0400

    Update Exercise_07.R

--- Exercise_07.R
+++ Exercise_07.R
@@ -6,12 +6,12 @@
 #your_file and read in your file 
 your_file <- "iris.csv"
 setwd("/Users/kevinmichael/Desktop/BioComp/data-shell/Biocomputing2020_Exercise07")
-df <- read.table(file=your_file, header=T, sep=",")
+dat <- read.table(file=your_file, header=T, sep=",")
 
 #now, set how many lines you would like to see
 #we use 7 as an arbitrary example
 lines <- 7
-head <- df[1:lines, ]
+head <- dat[1:lines, ]
 #now print 
 print(head)
 
@@ -28,9 +28,9 @@ print(dat[(nrow(dat)-1):nrow(dat), (ncol(dat)-1):ncol(dat)])
 
 # part B
 #to find the number of each species we subset on species and sum all occurences
-num_setosa <- sum(df$Species=="setosa")
-num_versicolor <- sum(df$Species=="versicolor")
-num_verginica <- sum(df$Species=="virginica")
+num_setosa <- sum(dat$Species=="setosa")
+num_versicolor <- sum(dat$Species=="versicolor")
+num_verginica <- sum(dat$Species=="virginica")
 
 # part C
 # Get rows with sepal.width >3.5
@@ -40,3 +40,9 @@ dat[dat[ ,2]>3.5, ]
 # write the data for the species setosa to a comma-delimited file named "setosa.csv"
 setosa_dat<-dat[dat[ ,5]=="setosa",]
 write.csv(setosa_dat,file="setosa.csv",row.names=FALSE)
+
+#to find the mean min and max, we subset on species virginica and find each parameter
+virginica <- subset(dat, df$Species=="virginica")
+mean(virginica$Petal.Length)
+min(virginica$Petal.Length)
+max(virginica$Petal.Length)
